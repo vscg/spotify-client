1ee14a4fa44e3a0ca3dc1249461d29eceab85625
nickdibari <ndibari@fordham.edu>
2020-08-21 20:48:22 -0400

    Add install_requires to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="spotify-client",
-    version="0.0.1",
+    version="0.0.2",
     author="MoodyTunes",
     author_email="moody.tunes.infrastucture@gmail.com",
     description="Client for interacting with the Spotify API",
@@ -13,6 +13,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Moody-Tunes/spotify-client",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'requests>=2.24.0',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
